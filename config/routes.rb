d6d11d794df320ccafc4b7c3cafbd415af082d22
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2020-03-24 19:43:45 +0900

    マイページ　天早

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"toppages#index"
   resources :toppages, only: [:index, :new, :create, :show]
-
+  resources :users, only: [:show]
 end
