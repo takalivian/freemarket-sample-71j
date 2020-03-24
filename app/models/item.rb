cfc2cecd04082e1688cdeae2d8531b71fccf563b
takalivian <61145524+takalivian@users.noreply.github.com>
2020-03-24 17:25:06 +0900

    fee　activehash

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,10 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :images
   
-
+  # activehash
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :statushash
+  belongs_to_active_hash :feehash
 
 
 end
