e3f5ff6060a6e312b2d81df5d766354900c0aea9
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2020-03-30 11:24:10 +0900

    サーバーサイド一覧表示機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,6 @@
 class ItemsController < ApplicationController
 
   def index
-    # @items = Item.order(created_at: :asc)
-    
     @items = Item.includes(:user).order("created_at desc").limit(6)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :email, :password, :first_name, :last_name, :birthday, :first_name_kana, :last_name_kana,
