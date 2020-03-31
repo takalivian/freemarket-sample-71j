c28e17bb3984b0628ff5659710f9bc47ba86a046
ieoon <science-pj@ezweb.ne.jp>
2020-03-31 15:50:21 +0900

    user model push

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :saler, class_name: "User"
+  belongs_to :buyer, class_name: "User"
   has_many :images, dependent: :destroy
   validates :images, :name,:text, :status, :fee, :prefecture_id, :shipping, :price, presence: true
   
